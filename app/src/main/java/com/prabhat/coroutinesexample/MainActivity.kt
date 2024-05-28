b37c3100a4de9e8074750bb64f925d59de6d8729
Prabhat kumar tiwari <sonukbc6@gmail.com>
2024-05-28 15:18:00 +0530

    Coroutiens jobs

--- app/src/main/java/com/prabhat/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/prabhat/coroutinesexample/MainActivity.kt
@@ -2,29 +2,39 @@ package com.prabhat.coroutinesexample
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.prabhat.coroutinesexample.databinding.ActivityMainBinding
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.withTimeoutOrNull
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding :ActivityMainBinding
-    private val RESULT_1="Result #1"
-    private val RESULT_2="Result #2"
-    private val TAG="ALEXA"
-//    private val JOB_TIMEOUT=1900L;
-    private val JOB_TIMEOUT=2100L;
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "ALEXA"
+
+    private val PROGRESS_MAX = 100
+    private val PROGRESS_START = 0
+    private val JOB_TIME = 4000
+    private lateinit var job: CompletableJob
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -32,73 +42,156 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.btn.setOnClickListener {
+        val rootView: View = findViewById(android.R.id.content)
+        showSnackbar(rootView,"hello")
 
+        binding.jobButton.setOnClickListener {
 
-//            io,main and default
-            CoroutineScope(Dispatchers.IO).launch{
-                fakeApiRequest()
+            //this way we check if lateinit value is intialized
+            if (!::job.isInitialized){
+
+                initJob()
             }
-
-        }}
-        private fun setNewText(input: String) {
-            val currentText = binding.textView.text.toString()
-            val newText = "$currentText\n$input"
-            binding.textView.text = newText
+            binding.jobProgressBar.startJobOrCancel(job)
         }
-
-        private suspend fun setTextOnMainThread(input: String){
-        withContext(Dispatchers.Main){
-            setNewText(input)
-        }
-
-    }
-
-
-    private suspend fun fakeApiRequest(){
-
-        withContext(Dispatchers.IO){
-
-          /*  val job=launch {
-                val result1=getResultFrom1Api()
-                setTextOnMainThread("Got ${result1}")
-                Log.d(TAG, "fakeApiRequest: $result1")
-
-                val result2=getResultFrom2Api()
-                setTextOnMainThread("Got ${result2}")
-            }*/
-            val job= withTimeoutOrNull(JOB_TIMEOUT) {
-                val result1=getResultFrom1Api()
-                setTextOnMainThread("Got ${result1}")
-                Log.d(TAG, "fakeApiRequest: $result1")
-
-                val result2=getResultFrom2Api()
-                setTextOnMainThread("Got ${result2}")
-            }
-            if (job==null){
-                val cancelMessage="Cancel Job You job took longer time $JOB_TIMEOUT"
-                setTextOnMainThread(cancelMessage)
+        val job2 = CoroutineScope(Dispatchers.IO).launch {
+            Log.d(TAG, "onCreate: Job2 STARTED ")
+            try {
+                delay(5000)  // Simulate a long-running task
+            } catch (e: CancellationException) {
+                Log.d(TAG, "onCreate: Job2 CANCELLED")
             }
         }
 
+        val job3 = CoroutineScope(Dispatchers.IO).launch {
+            Log.d(TAG, "onCreate: Job3 STARTED")
+            try {
+                delay(5000)  // Simulate a long-running task
+            } catch (e: CancellationException) {
+                Log.d(TAG, "onCreate: Job3 CANCELLED")
+            }
+        }
+        val scope=CoroutineScope(IO).launch {
+
+            Log.d(TAG, "onCreate: scope STARTED")
+            try {
+                delay(5000)  // Simulate a long-running task
+            } catch (e: CancellationException) {
+                Log.d(TAG, "onCreate: scope CANCELLED")
+            }
+
+        }
+        CoroutineScope(IO).launch {
+            Log.d(TAG, "onCreate: scope2 STARTED")
+            try {
+                delay(5000)  // Simulate a long-running task
+            } catch (e: CancellationException) {
+                Log.d(TAG, "onCreate: scope2 CANCELLED")
+            }
+        }
+
+        binding.btnCancelJob.setOnClickListener {
+
+            scope.cancel()
+        }
+
+      /*  binding.btnCancelJob.setOnClickListener {
+            Log.d(TAG, "onCreate: cancel button pressed")
+            if(job2.isActive){
+
+                job2.cancel(CancellationException("job 2 cancelled"))
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(1000)
+                Log.d(TAG, "onCreate: cancel button pressed inside coroutine")
+                if (job2.isCancelled) {
+                    Log.d(TAG, "onCreate: Job2 is cancelled")
+                } else if (job2.isActive) {
+                    Log.d(TAG, "onCreate: Job2 is Active")
+                }
+                if (job3.isCancelled) {
+                    Log.d(TAG, "onCreate: Job3 is cancelled")
+                } else if (job3.isActive) {
+                    Log.d(TAG, "onCreate: Job3 is Active")
+                }
+            }
+        }*/
 
     }
-    private suspend fun getResultFrom1Api():String{
 
-        logThread("getResultFrom1Api")
-        delay(1000)
-        return RESULT_1
+    fun ProgressBar.startJobOrCancel(job:Job){
+
+        if(this.progress>0){
+            Log.d(TAG, "${job} is already active Cancelling ")
+            resetJob()
+        }else{
+            binding.jobButton.text="Cancel job #1"
+
+           /* val scope=CoroutineScope(IO).launch {
+
+            }
+            CoroutineScope(IO).launch {  }
+
+            scope.cancel()//this will cancel all the all job with IO scope*/
+
+            CoroutineScope(IO+job).launch {
+
+                Log.d(TAG, "Coroutines $this is activated with job $job ")
+                for (i in PROGRESS_START..PROGRESS_MAX){
+                    delay((JOB_TIME/PROGRESS_MAX).toLong())
+                    this@startJobOrCancel.progress=i
+                }
+                updateJobCompleteTextView("Job is complete")
+            }
+        }
+        
     }
-    private suspend fun getResultFrom2Api():String{
-
-        logThread("getResultFrom2Api")
-        delay(1000)
-        return RESULT_2
+    private fun updateJobCompleteTextView(input:String){
+        CoroutineScope(Main).launch {
+            binding.jobCompleteText.text=input
+        }
     }
-    private fun logThread(methodName:String){
-
-        Log.d(TAG, "logThread:${methodName}  ${Thread.currentThread().name} ")
 
 
+    private fun resetJob() {
+
+        if (job.isActive||job.isCompleted){
+            job.cancel(CancellationException("Resetting job"))
+        }
+        initJob()
     }
+
+
+    fun initJob() {
+        binding.jobButton.text = "Start Job #1"
+        updateJobCompleteTextView("")
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrBlank()) {
+                    msg = "Unknown Cancellation error"
+                }
+                Log.d(TAG, "${job}: was cancelled because $msg ")
+                showToast(msg)
+
+            }
+        }
+        binding.jobProgressBar.max=PROGRESS_MAX
+        binding.jobProgressBar.progress=PROGRESS_START
+    }
+
+    // Function to show a Snackbar
+    fun showSnackbar(view: View, message: String) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun showToast(input: String) {
+        GlobalScope.launch(Dispatchers.Main) {
+
+            Toast.makeText(this@MainActivity, input, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
